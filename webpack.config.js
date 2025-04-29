af724eb7fe7a41dc4c07c9d144cf1b683ee075f0
Wayne <34191804+wkoh1024@users.noreply.github.com>
2025-04-29 14:37:21 -0400

    Initial commit + setting up plugins and packages

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+}
