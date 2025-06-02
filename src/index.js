be561179a509a1d896551e12a872cc61c444dec8
Wayne <waynekoh1024@gmail.com>
2025-06-02 16:46:28 -0400

    Clears content before each loads

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import loadHome from "./home.js";
+import loadMenu from "./menu.js";
 import "./styles.css";
 
-// document.querySelector("#content").textContent = ""; //  clears content
 
 let buttons = document.querySelectorAll("nav button");
-console.log(buttons);
 
-// loadHome();
+let clearContent = () => {
+    document.querySelector("#content").textContent = "";
+}; //  clears content
+
+loadHome();
 
 buttons.forEach(button => {
     button.addEventListener('click', (e)=> {
         switch(e.target.textContent) {
             case 'Home':
+                clearContent();
                 loadHome();
                 break;
             case 'Menu':
+                clearContent();
+                loadMenu();
                 break;
             case 'About':
-                
+                clearContent();
                 console.log("asd");
                 break;
             default:
